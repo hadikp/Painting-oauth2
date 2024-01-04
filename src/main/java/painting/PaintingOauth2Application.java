db2f4d17680dacae1c6c01b598be9b4237e09c4f
hadikp <hp20001025@gmail.com>
2024-01-04 21:42:04 +0100

    first commit

---
+++ src/main/java/painting/PaintingOauth2Application.java
@@ -0,0 +1,13 @@
+package painting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintingOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintingOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/painting/PaintingOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package painting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintingOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
