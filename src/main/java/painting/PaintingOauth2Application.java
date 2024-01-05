68d59306d1f40d22d4861d6a0861a61be752fa77
hadikp <hp20001025@gmail.com>
2024-01-05 18:54:07 +0100

    oauth2 works

--- src/main/java/painting/PaintingOauth2Application.java
+++ src/main/java/painting/PaintingOauth2Application.java
@@ -2,7 +2,10 @@ package painting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import painting.token.RsaKeyProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class PaintingOauth2Application {
 
