ed4ba865b335df9ab9550f062bd7701a52c0bdec
hadikp <hp20001025@gmail.com>
2024-01-09 19:03:41 +0100

    add cors Config

--- src/main/java/painting/PaintingOauth2Application.java
+++ src/main/java/painting/PaintingOauth2Application.java
@@ -1,5 +1,6 @@
 package painting;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,6 +12,10 @@ public class PaintingOauth2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaintingOauth2Application.class, args);
+	}
+
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
